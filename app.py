16ea652ce7b5f3337a00f8c00199da5cf3e39d54
yay <amy.lian314159@gmail.com>
2026-05-04 21:46:45 +1200

    debugged by fixing get_db function

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 
 def get_db():
     db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
@@ -18,7 +25,7 @@ def home():
     db.row_factory = sqlite3.Row 
     cursor = db.cursor()
 
-    sql = "SELECT image_name, item_type, price, is_available, item_photo " \
+    sql = "SELECT item_name, item_type, price, is_available, item_photo " \
     "FROM Menu;"
 
     cursor.execute(sql)
@@ -31,3 +38,6 @@ def query_db(query, args=(), one=False):
     rv = cursor.fetchall()
     cursor.close()
     return (rv[0] if rv else None) if one else rv
+
+if __name__ == "__main__":
+    app.run(debug=True)
